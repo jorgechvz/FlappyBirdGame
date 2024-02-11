47b537719b3c34386bef2cc54570f48c6e8dfd9c
jorgechvz <jorgechvzpon@gmail.com>
2024-02-10 23:35:41 -0500

    Add Flappy Bird sound effects and set difficulty level

--- settings.py
+++ settings.py
@@ -16,3 +16,15 @@ class Settings:
         self.pip_distance = 300
         self.pipe_frequency = 1500
         self.last_pipe = pygame.time.get_ticks() - self.pipe_frequency
+        self.pass_pipe = False
+        self.score = 0
+        
+        # Flappy Bird Songs
+        self.hit_sound = pygame.mixer.Sound('assets/songs/hit.wav')
+        self.point_sound = pygame.mixer.Sound('assets/songs/point.wav')
+        self.wing_sound = pygame.mixer.Sound('assets/songs/wing.wav')
+        
+        # Set level of difficulty
+        """ self.levels = {1: 5, 2: 10, 3: 15}
+        self.level = 1
+         """
