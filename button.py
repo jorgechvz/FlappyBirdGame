103c52542107f4cdf8a6f07803defe612438ed6d
jorgechvz <jorgechvzpon@gmail.com>
2024-02-11 00:09:36 -0500

    Add constructor and draw method to Button class and comments

--- button.py
+++ button.py
@@ -1,11 +1,13 @@
 import pygame
 
 class Button():
+    # Constructor for the Button class that takes in the x and y coordinates of the button and the image of the button
 	def __init__(self, x, y, image):
 		self.image = image
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
-
+	
+	# Method to draw the button on the screen and check if the button is clicked on by the user 
 	def draw(self, screen):
 
 		action = False
